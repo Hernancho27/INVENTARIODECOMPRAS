0523dd96c6c09872244d314fbd119ddc89995788
DiplomadoUNAD <hernancho.rojas@yahoo.com>
2017-05-08 22:26:48 -0500

    Se adiciona DrawerMenu, falta configurarlo y animarlo correctamente

--- app/src/main/java/codigohernancho/app/prueba/com/inventariodecompras/INICIO.java
+++ app/src/main/java/codigohernancho/app/prueba/com/inventariodecompras/INICIO.java
@@ -1,11 +1,17 @@
 package codigohernancho.app.prueba.com.inventariodecompras;
 
 import android.content.DialogInterface;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
+
 
 public class INICIO extends AppCompatActivity implements View.OnClickListener{
 
@@ -20,6 +26,10 @@ public class INICIO extends AppCompatActivity implements View.OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+
         boton1 = (Button)findViewById(R.id.boton1);
         boton1.setOnClickListener(this);
         boton2 = (Button)findViewById(R.id.boton2);
@@ -30,7 +40,18 @@ public class INICIO extends AppCompatActivity implements View.OnClickListener{
         boton4.setOnClickListener(this);
         boton5 = (Button)findViewById(R.id.boton5);
         boton5.setOnClickListener(this);
-    }
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+                                   @Override
+                                   public void onClick(View view) {
+                                       Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                                               .setAction("Action", null).show();
+                                   }
+        });
+
+
+    };
 
     @Override
     public void onClick(View v) {
@@ -58,4 +79,23 @@ public class INICIO extends AppCompatActivity implements View.OnClickListener{
         }
 
     }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_inicio, menu);
+        return true;}
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
+
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
+                return true;
+            }
+
+            return super.onOptionsItemSelected(item);
+        }
 }
