0d44c41a82455f473d304e85a79aa1b062d5705f
Hernancho27 <hernancho.rojas@yahoo.com>
2017-05-14 22:49:11 -0500

    Commit

--- app/src/main/java/codigohernancho/app/prueba/com/inventariodecompras/DrawerMenu.java
+++ app/src/main/java/codigohernancho/app/prueba/com/inventariodecompras/DrawerMenu.java
@@ -82,17 +82,17 @@ public class DrawerMenu extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.crear) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.entradas) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.salidas) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.alertas) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.informes) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.configuracion) {
 
         }
 
