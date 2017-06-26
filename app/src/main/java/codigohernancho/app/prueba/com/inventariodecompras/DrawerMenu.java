20062c4c525811c1537dfb3e808d1b38f3c5db33
DiplomadoUNAD <hernancho.rojas@yahoo.com>
2017-06-25 21:20:42 -0500

    Se realiza reorganizacion de la funcionalidad Bloc de notas.

--- app/src/main/java/codigohernancho/app/prueba/com/inventariodecompras/DrawerMenu.java
+++ app/src/main/java/codigohernancho/app/prueba/com/inventariodecompras/DrawerMenu.java
@@ -60,7 +60,7 @@ public class DrawerMenu
 
         bt.setOnClickListener(this);
                 /*ejemplos*/
-        manager.insertar("1236547","Arroz","Arroz Diana");;
+        manager.insertar("1236547","Arroz","Arroz Diana");
         manager.insertar("789654","Maiz","Maiz tierno en arina");
         manager.insertar("456987","Azucar","Azucar Morena");
 
